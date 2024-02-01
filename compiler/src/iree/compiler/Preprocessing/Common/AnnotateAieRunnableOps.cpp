6c725b31e045f55d0afe83dc263e7dca9b916af8
Sungsoon Cho <sungsoon.cho@gmail.com>
2024-01-31 21:46:01 -0800

    Compiles and links.

--- compiler/src/iree/compiler/Preprocessing/Common/AnnotateAieRunnableOps.cpp
+++ compiler/src/iree/compiler/Preprocessing/Common/AnnotateAieRunnableOps.cpp
@@ -1,10 +1,24 @@
+#include "iree/compiler/Preprocessing/Common/PassDetail.h"
+#include "iree/compiler/Preprocessing/Common/Passes.h"
 #include "mlir/Pass/Pass.h"
 
 namespace mlir::iree_compiler::Preprocessing {
 
+namespace {
+
+// TODO(sungsoon)
+struct AnnotateAieRunnableOpsPass
+    : AnnotateAieRunnableOpsBase<AnnotateAieRunnableOpsPass> {
+  void runOnOperation() override {
+    printf("xxxxxxxx AnnotateAieRunnableOpsPass\n");
+    fprintf(stderr, "yyyyyyyy AnnotateAieRunnableOpsPass\n");
+  }
+};
+
+} // namespace
+
 std::unique_ptr<Pass> createAnnotateAieRunnableOpsPass() {
-  // TODO(sungsoon)
-  return nullptr;
+  return std::make_unique<AnnotateAieRunnableOpsPass>();
 }
 
 } // namespace mlir::iree_compiler::Preprocessing
